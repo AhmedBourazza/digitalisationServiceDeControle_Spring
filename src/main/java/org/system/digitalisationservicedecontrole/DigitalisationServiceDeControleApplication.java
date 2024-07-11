9a118de560e94b3069a7cec940de954f6a440fe2
AhmedBourazza <ahmed.lesnar2002@gmail.com>
2024-07-11 19:25:11 +0100

    first commit

---
+++ src/main/java/org/system/digitalisationservicedecontrole/DigitalisationServiceDeControleApplication.java
@@ -0,0 +1,13 @@
+package org.system.digitalisationservicedecontrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalisationServiceDeControleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalisationServiceDeControleApplication.class, args);
+    }
+
+}
