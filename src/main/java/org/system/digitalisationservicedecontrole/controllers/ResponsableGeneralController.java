2f9a906779ccbcbbc87f8b8cdc78e83d3ad0b8c2
AhmedBourazza <ahmed.lesnar2002@gmail.com>
2024-07-12 16:53:57 +0100

    ajout de l espace responsable general

---
+++ src/main/java/org/system/digitalisationservicedecontrole/controllers/ResponsableGeneralController.java
@@ -0,0 +1,27 @@
+package org.system.digitalisationservicedecontrole.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ResponsableGeneralController {
+
+    @GetMapping("/responsableGeneral/listeControleurs")
+    public String Controleurs() {
+        return "RG_ListeControleurs"; // Assurez-vous que "C_listeEquipements.html" est présent dans le dossier templates
+    }
+
+    @GetMapping("/responsableGeneral/dashboard")
+    public String dashboard() {
+        return "RG_dashboard";
+    }
+    @GetMapping("/responsableGeneral/editProfile")
+    public String EditProfile() {
+        return "RG_editProfile"; // Assurez-vous que "C_listeEquipements.html" est présent dans le dossier templates
+    }
+
+    @GetMapping("/responsableGeneral/monProfile")
+    public String MonProfile() {
+        return "RG_monProfile"; // Assurez-vous que "C_listeEquipements.html" est présent dans le dossier templates
+    }
+}
