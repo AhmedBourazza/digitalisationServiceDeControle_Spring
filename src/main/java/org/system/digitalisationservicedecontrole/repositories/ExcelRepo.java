9827e4141cd192c180220fd794f22bf6f6f72c98
ilyasmakh <ilyasmakhloul1@gmail.com>
2024-07-24 20:06:08 +0100

    triglat Excel pour nv BD

--- src/main/java/org/system/digitalisationservicedecontrole/repositories/ExcelRepo.java
+++ src/main/java/org/system/digitalisationservicedecontrole/repositories/ExcelRepo.java
@@ -11,8 +11,9 @@ public interface ExcelRepo extends CrudRepository<Excel, Long> {
     @Query(value = "SELECT f.date_controle, e.nom, u.nom, s.nom, q.enonce, r.enonce , r.justification,c.username,f.proprietaire " +
             "FROM reponse r " +
             "JOIN question q ON r.id_question_ = q.id_question " +
+            "JOIN formulaire f ON r.id_formulaire_ = f.id_formulaire " +
             "JOIN section s ON q.id_section_ = s.id_section " +
-            "JOIN formulaire f ON s.id_formulaire_ = f.id_formulaire " +
+            "JOIN equipement eq ON s.id_equipement_ = eq.id_equipement " +
             "JOIN controleur c ON f.id_controleur_  = c.id_controleur  " +
             "JOIN unite u ON f.id_unite_ = u.id_unite " +
             "JOIN entite e ON u.id_entite_ = e.id_entite " +
